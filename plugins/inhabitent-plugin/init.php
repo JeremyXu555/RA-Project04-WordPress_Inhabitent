d9625e511998e1b7022acd29a0a0e036fd2e353d
HoverEagle <jeremyxu89@gmail.com>
2017-03-07 16:16:42 -0500

    Style for the products archive page and add plugin.

---
+++ plugins/inhabitent-plugin/init.php
@@ -0,0 +1,20 @@
+ <?php 
+
+/**
+ * Plugin Name: Inhabitent
+ * Plugin URI: https://github.com/JeremyXu555
+ * Description: Inhabitent Plugin
+ * Version: 1.0.0
+ * Author: Jeremy Xu
+ * Author URI: https://github.com/JeremyXu555
+ * License: GPL2
+ */
+
+ if ( ! defined( 'WPINC' ) ) {
+  die;
+	}
+
+	define( 'RF_DIR', dirname( __FILE__ ) );
+	include(RF_DIR . "/lib/post_tax.php");
+
+?>
